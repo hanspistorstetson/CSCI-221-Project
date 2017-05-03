a85485f63fa0169a9dd7149e4d408cc99e56fdee
John Sawyer <jsawyer4@live.com>
2017-05-03 14:15:30 -0400

    added code for the database

--- gui/activitysearch.cpp
+++ gui/activitysearch.cpp
@@ -2,12 +2,19 @@
 #include "ui_activitysearch.h"
 #include "gui/activitywindow.h"
 #include "gui/listactivities.h"
+#include "database/activity.h"
 ActivitySearch::ActivitySearch(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ActivitySearch)
 {
     ui->setupUi(this);
-
+    /*std::vector<Activity*> eventActivity;
+     eventActivity=Activity::getAllActivities();
+     for (int t = 0; t<eventActivity.size();t++)
+     {
+         ui->listWidget->addItem(eventActivity.at(t).getActivityName());
+     }
+     */
 }
 
 ActivitySearch::~ActivitySearch()
@@ -17,6 +24,7 @@ ActivitySearch::~ActivitySearch()
 
 void ActivitySearch::on_listWidget_itemClicked(QListWidgetItem *item)
 {
+
     ActivityWindow la;
     this->hide();
     la.setModal(true);
@@ -27,4 +35,10 @@ void ActivitySearch::on_listWidget_itemClicked(QListWidgetItem *item)
 void ActivitySearch::on_back_released()
 {
     this->close();
+}
+
+void ActivitySearch::on_searchButton_released()
+{
+    ui->listWidget->clear();
+    ui->listWidget->addItem(ui->lineEdit->text());
 }
